604cc328c8aaab2b0d0f75db706445a8d1002265
Ayush Sharma <ayush@theflak.in>
2021-06-03 21:11:30 +0530

    FIxed all isuues

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@ const submitForm = () => {
   return false;
 };
 
-const switchState = (type, device) => {
+const cameraAudioToggleState = (type, device) => {
   if (type.classList.contains("onClass")) {
-      if(device.toLowerCase() === 'video'){
-          startVideo()
-      }
+    if (device.toLowerCase() === "video") {
+      stopVideo();
+    }
     type.classList.remove("onClass");
     type.src = `./assets/img/${device}Off.png`;
   } else {
-      stopVideo()
+    if (device.toLowerCase() === "video") {
+      startVideo();
+    }
     type.classList.add("onClass");
     type.src = `./assets/img/${device}On.png`;
   }
@@ -18,27 +20,46 @@ const switchState = (type, device) => {
 
 const startVideo = () => {
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
-      });
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    });
   }
 };
 
 const stopVideo = () => {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.pause()
-      });
+  const video = document.querySelector("video");
+  const mediaStream = video.srcObject;
+  const tracks = mediaStream.getTracks();
+  tracks[0].stop();
+  tracks.forEach((track) => track.stop());
+};
+
+const showDropdown = (type, whatToShow, whatNotToShow, whatToHide, device) => {
+  if (type.classList.contains("noDropdown")) {
+    whatToShow.style.color = "#fff";
+    if (device == "camera") {
+      whatNotToShow.style.color = "";
+      whatToShow.childNodes[1].src = "./assets//img/down.png";
+      whatNotToShow.childNodes[1].src = "./assets//img/next.png";
+      type.classList.remove("noDropdown");
+      whatToHide.classList.add("noDropdown");
+    } else {
+      whatNotToShow.style.color = "";
+      whatToShow.childNodes[1].src = "./assets//img/down.png";
+      whatNotToShow.childNodes[1].src = "./assets//img/next.png";
+      type.classList.remove("noDropdown");
+      whatToHide.classList.add("noDropdown");
+    }
+  } else {
+    whatToShow.style.color = "";
+    if (device == "camera") {
+      whatToShow.childNodes[1].src = "./assets//img/next.png";
+      type.classList.add("noDropdown");
+    } else {
+      whatToShow.childNodes[1].src = "./assets//img/next.png";
+      type.classList.add("noDropdown");
+    }
   }
 };
 
